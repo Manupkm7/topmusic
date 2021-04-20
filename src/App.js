dfe6275ec2835e260fb73f988e73338a0f20b5b2
Manuel Espinoza <espinoza-manuel36@hotmail.com>
2021-04-19 22:02:01 -0300

    app de musica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import {useState, useEffect} from 'react';
+import Player from './components/Player';
+  
+  function App() {
+    const [songs] = useState([{
+      title: "Shy Away",
+      artist: "Twenty One Pilots",
+      img_src: "./images/shyaway.jpg",
+      src: "./music/shy-away.mp3"
+    },
+    {
+      title: "Stressed Out",
+      artist: "Twenty One Pilots",
+      img_src: "./images/stressedout.jpg",
+      src: "./music/stressed-out.mp3"
+    },
+    {
+      title: "My Blood",
+      artist: "Twenty One Pilots",
+      img_src: "./images/myblood.jpg",
+      src: "./music/my-blood.mp3"
+    },
+    {
+      title: "Trees",
+      artist: "Twenty One Pilots",
+      img_src: "./images/trees.jpg",
+      src: "./music/trees.mp3"
+    },
+    {
+      title: "Lovely",
+      artist: "Twenty One Pilots",
+      img_src: "./images/lovely.jpg",
+      src: "./music/lovely.mp3"
+    },
+    {
+      title: "Friend, Please",
+      artist: "Twenty One Pilots",
+      img_src: "./images/friendplease.jpg",
+      src: "./music/friend-please.mp3"
+    }
+  ])
+  
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const [nextSongIndex, setNextSongIndex] = useState(0);
+  
+    useEffect(() => {
+      setNextSongIndex(() => {
+        if (currentSongIndex + 1 > songs.length - 1) {
+          return 0;
+        } else {
+          return currentSongIndex + 1;
+        }
+      });
+    }, [currentSongIndex]);
+  
+    return (
+      <div className="App">
+        <Player 
+          currentSongIndex={currentSongIndex} 
+          setCurrentSongIndex={setCurrentSongIndex} 
+          nextSongIndex={nextSongIndex} 
+          songs={songs}
+        />
+      </div>
+    );
+  }
+  
+  export default App;
